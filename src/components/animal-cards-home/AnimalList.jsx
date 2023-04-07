ba5dc7dd819de00f33802ff58d0a1e84f58e89de
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-07 17:07:43 +0200

    animal info done

--- src/components/animal-cards-home/AnimalList.jsx
+++ src/components/animal-cards-home/AnimalList.jsx
@@ -8,26 +8,31 @@ function AnimalList() {
         name: 'Simba',
         species: 'Lion',
         age: 6,
-        image: 'https://unsplash.com/fr/photos/2vbhN2Yjb3A'
+        image: 'https://cdn.pixabay.com/photo/2015/09/22/14/34/lion-951778_1280.jpg'
     };
 
     const panda = {
         name: 'Bambou',
         species: 'Panda',
         age: 3,
-        image: 'https://unsplash.com/fr/photos/s-fD5Tpew2k'
+        image: 'https://cdn.pixabay.com/photo/2013/11/20/23/02/panda-214260_1280.jpg'
     };
 
     const butterfly = {
         name: 'Yoni',
         species: 'Butterfly',
         age: 'unknown',
-        image: 'https://unsplash.com/fr/photos/rVGr8zm6K7I'
+        image: 'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg'
     };
 
     return (
-        <div>
-            
+        <div className='AnimalList'>
+            <h2 className='section-title AnimalList-title'>Venez faire de belles rencontres !</h2>
+            <div className='card-container'>
+                <Animal {...lion}/>
+                <Animal {...panda}/>
+                <Animal {...butterfly}/>
+            </div>
         </div>
     )
 }
