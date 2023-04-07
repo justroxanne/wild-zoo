6ac982e0d39232c064375e6e4b18645099f6e30f
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-07 16:27:05 +0200

    animals objects done

---
+++ src/components/animal-cards-home/AnimalList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Animal from './Animal';
+import './AnimalList.css'
+
+function AnimalList() {
+
+    const lion = {
+        name: 'Simba',
+        species: 'Lion',
+        age: 6,
+        image: 'https://unsplash.com/fr/photos/2vbhN2Yjb3A'
+    };
+
+    const panda = {
+        name: 'Bambou',
+        species: 'Panda',
+        age: 3,
+        image: 'https://unsplash.com/fr/photos/s-fD5Tpew2k'
+    };
+
+    const butterfly = {
+        name: 'Yoni',
+        species: 'Butterfly',
+        age: 'unknown',
+        image: 'https://unsplash.com/fr/photos/rVGr8zm6K7I'
+    };
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AnimalList;
