4937859a6d4c0bd55bccb9996f948a69de3e2a20
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-07 11:29:30 +0200

    menu link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,11 @@ function Navbar() {
     return (
         <div className="Navbar">
             <a href="/"><img className="logo" src={logo} alt="Zoo loo"/></a>
+            <div className="navigation">
+                <a className="navigation-link" href="">NOS ANIMAUX</a>
+                <a className="navigation-link" href="/">À PROPOS</a>
+                <a className="navigation-link" href="/">CONTACT</a>
+            </div>
         </div> 
     )
 }
